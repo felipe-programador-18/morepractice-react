0eda5edf1c8c880e0915bb0d6d26b625ba69196c
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-17 15:35:24 -0300

    c1- adding first modify

--- pratice-app/src/App.js
+++ pratice-app/src/App.js
@@ -1,11 +1,26 @@
 
 import './App.css';
 
+
+
+//remmeber little bit about react and flag used usestate
+const Paragrafo = (props) => {
+  return (
+    <p>{props.text} </p>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
       <h1>Lets jump right now!!</h1>       
-    </div>
+    
+    <Paragrafo text='thought more and practice double down' />
+    
+    
+    
+      </div>
   );
 }
 
