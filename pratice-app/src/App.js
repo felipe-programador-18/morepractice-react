53a9dcf665786b629ece692b3c70074248e494cd
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-10 08:12:39 -0300

    c10- teste

--- pratice-app/src/App.js
+++ pratice-app/src/App.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react'
+import React , {useState, useReducer, useEffect} from 'react'
 import './App.css'
 import Button from './button'
 import H1 from './h1'
@@ -6,12 +6,48 @@ import H3 from './h3'
 import Paragrafo from './paragr'
 import axios from 'axios'
 
+//remmerber to caught url i have always remmeber caught api about axios
+
+const Url ="https://testeapp-practice-default-rtdb.firebaseio.com/movimentacoes.json"
+
+//remember reducer function always receive state and action to caught API REST!!
+const reducer = (state, action) =>{
+  // this is if and if to caught anf restapi
+  if(action.type === 'REQUEST'){
+    return{
+      ...state,
+      loading: true
+    }
+  }
+  if(action.type === 'SUCCESS'){
+    return {
+      ...state,
+      loading: false,
+      data: action.data
+    }
+  }
 
 
+  return state
+}
 
 function App() {
  const [moreone, setmoreone] = useState(0)
-  
+ 
+ // here i always set usereducer to dispacth and caught date
+ const [data, dispatch] = useReducer(reducer,({ loading: true,data:{}}))
+
+// i always make and use usereducer because to seeing and show in the dashboard
+useEffect(() =>{
+   dispatch({type:'REQUEST'})
+  axios
+  .get(Url)
+  .then(res =>{
+     dispatch({type:'SUCCESS', data: res.data})
+  })
+}, [])
+
+
  const Increased = () => {
    setmoreone(moreone+1)
  } 
@@ -40,10 +76,10 @@ function App() {
 
 
 
-      
-
       <h1>Test api here now</h1>
 
+          {JSON.stringify(data)}
+          {data.loading && <p>Loading......</p>}
       </div>
   );
 }
