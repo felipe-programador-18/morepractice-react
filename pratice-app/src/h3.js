1a04c02fc8763100f69946f0d93b79f2fd96dadb
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-26 10:21:55 -0300

    c19- practice more about reactfragment

--- pratice-app/src/h3.js
+++ pratice-app/src/h3.js
@@ -3,8 +3,13 @@ import React from "react";
 
 const H3 = (props) =>{
     return (
+    <React.Fragment>
       <h3> {props.kinda} </h3>
-    )
+      <p> more more more</p>
+      <h5> try it more and more</h5>
+    
+      </React.Fragment>
+      )
    }
 
 
